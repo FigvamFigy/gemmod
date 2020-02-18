fa92ecd584d0187679330eb4ffdca8a4195532c9
FigvamFigy <looyloop123@gmail.com>
2020-02-18 09:34:05 -0800

    Adds the two block tall AABB (bounding box) to statue

--- src/main/java/com/figvam/gemmod/blocks/statue/BlockStatue.java
+++ src/main/java/com/figvam/gemmod/blocks/statue/BlockStatue.java
@@ -2,10 +2,19 @@ package com.figvam.gemmod.blocks.statue;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class BlockStatue extends Block {
 
+
+    private static final AxisAlignedBB STATUE_AABB = new AxisAlignedBB(0,0,0,1,2,1);
 
     private static BlockStatue instance = null;
 
@@ -28,4 +37,15 @@ public class BlockStatue extends Block {
 
 
 
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos){
+        return STATUE_AABB;
+    }
+
+
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return STATUE_AABB;
+    }
 }
