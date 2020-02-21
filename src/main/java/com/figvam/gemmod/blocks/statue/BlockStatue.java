c60467d105fc8afa978a097bb9976d4ab0f9618a
FigvamFigy <looyloop123@gmail.com>
2020-02-21 15:06:20 -0800

    Adds statue orientation

--- src/main/java/com/figvam/gemmod/blocks/statue/BlockStatue.java
+++ src/main/java/com/figvam/gemmod/blocks/statue/BlockStatue.java
@@ -1,9 +1,16 @@
 package com.figvam.gemmod.blocks.statue;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -15,6 +22,7 @@ public class BlockStatue extends Block {
 
 
     private static final AxisAlignedBB STATUE_AABB = new AxisAlignedBB(0,0,0,1,2,1);
+    private static PropertyDirection PROPERTY_DIRECTION = BlockHorizontal.FACING;
 
     private static BlockStatue instance = null;
 
@@ -58,4 +66,48 @@ public class BlockStatue extends Block {
     public boolean isFullCube(IBlockState state){
         return false;
     }
+
+
+
+    //meta data stuff and state stuff
+    @Override
+    public IBlockState getStateFromMeta(int meta){
+        switch (meta){
+            case 1: return this.getDefaultState().withProperty(PROPERTY_DIRECTION,EnumFacing.NORTH);
+            case 2: return this.getDefaultState().withProperty(PROPERTY_DIRECTION,EnumFacing.SOUTH);
+            case 3: return this.getDefaultState().withProperty(PROPERTY_DIRECTION,EnumFacing.WEST);
+            default: return this.getDefaultState().withProperty(PROPERTY_DIRECTION,EnumFacing.EAST);
+        }
+
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state){
+        EnumFacing currentFacing = state.getValue(PROPERTY_DIRECTION);
+
+        switch(currentFacing){
+            case NORTH: return 1;
+            case SOUTH: return 2;
+            case WEST: return 3;
+            default: return 4;
+        }
+
+    }
+
+
+    //this will create a new blockstate and we pass all the properties that exist
+    @Override
+    public BlockStateContainer createBlockState(){
+        return new BlockStateContainer(this, new IProperty[]{PROPERTY_DIRECTION});
+    }
+
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        worldIn.setBlockState(pos,state.withProperty(PROPERTY_DIRECTION,placer.getHorizontalFacing().getOpposite()),2);
+
+    }
+
+
+
+
 }
