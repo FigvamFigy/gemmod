53d2f64a2842393cfb1871212596a0437c6cd58a
FigvamFigy <looyloop123@gmail.com>
2020-02-28 19:54:58 -0800

    Adds functional tile entity counter

--- src/main/java/com/figvam/gemmod/blocks/doubleFurance/BlockDoubleFurnace.java
+++ src/main/java/com/figvam/gemmod/blocks/doubleFurance/BlockDoubleFurnace.java
@@ -1,8 +1,21 @@
 package com.figvam.gemmod.blocks.doubleFurance;
 
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import org.lwjgl.Sys;
+
+import javax.annotation.Nullable;
 
 public class BlockDoubleFurnace extends Block {
 
@@ -27,4 +40,33 @@ public class BlockDoubleFurnace extends Block {
     }
 
 
+    //Tile Entity stuffs
+    @Override
+    public boolean hasTileEntity(IBlockState state){
+        return true;
+    }
+
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(World world, IBlockState state) {
+        //return super.createTileEntity(world, state);
+        return DoubleFurnaceTileEntity.getInstance();
+    }
+
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if(!worldIn.isRemote){//This says, if the side is the PHYSICAL SERVER, NOT ANY CLIENTS, then do logic
+            DoubleFurnaceTileEntity tile = DoubleFurnaceTileEntity.getTileEntity(worldIn,pos);
+
+            //DoubleFurnaceTileEntity tile = (DoubleFurnaceTileEntity)worldIn.getTileEntity(pos);
+            tile.incrementCount();
+            playerIn.sendMessage(new TextComponentString("Count: " + tile.getCount()));
+
+        }
+
+
+        return true;
+    }
 }
--- src/main/java/com/figvam/gemmod/blocks/doubleFurance/DoubleFurnaceTileEntity.java
+++ src/main/java/com/figvam/gemmod/blocks/doubleFurance/DoubleFurnaceTileEntity.java
@@ -1,14 +1,20 @@
 package com.figvam.gemmod.blocks.doubleFurance;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class DoubleFurnaceTileEntity extends TileEntity {
 
+
+    private int count;
 
     private static DoubleFurnaceTileEntity instance = null;
 
     private DoubleFurnaceTileEntity(){
         super();
+        //count = 0;
     }
 
     public static DoubleFurnaceTileEntity getInstance(){
@@ -18,6 +24,43 @@ public class DoubleFurnaceTileEntity extends TileEntity {
         return instance;
     }
 
+
+    public static DoubleFurnaceTileEntity getTileEntity(IBlockAccess world, BlockPos pos){
+
+        return (DoubleFurnaceTileEntity)world.getTileEntity(pos);
+    }
+
+    //Saving data inside a tile entity
+
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+
+        compound.setInteger("count",count);
+
+        return super.writeToNBT(compound);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        count = compound.getInteger("count");
+
+        super.readFromNBT(compound);
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void incrementCount(){
+        count++;
+        markDirty();
+    }
+
+    private void decrementCount(){
+        count--;
+        markDirty();
+    }
 
 
 }
